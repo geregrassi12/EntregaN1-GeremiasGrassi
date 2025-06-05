a02feed06af9f21d2897ba2a9ce3b059d41dc62a
Geremias Grassi <geremias.grassi@gmail.com>
2025-06-04 23:01:40 -0300

    Otros arreglos

--- src/app.js
+++ src/app.js
@@ -5,14 +5,11 @@ import cartsRouter from './routes/carts.routes.js';
 const app = express();
 const PORT = 8080;
 
-// Middleware para parsear JSON
 app.use(express.json());
 
-// Rutas
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-// Levantar servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -4,20 +4,17 @@ import { CartManager } from '../managers/CartManager.js';
 const router = Router();
 const manager = new CartManager();
 
-// POST /api/carts/ → crear carrito
 router.post('/', async (req, res) => {
   const newCart = await manager.createCart();
   res.status(201).json(newCart);
 });
 
-// GET /api/carts/:cid → ver productos de un carrito
 router.get('/:cid', async (req, res) => {
   const cart = await manager.getCartById(req.params.cid);
   if (cart) res.json(cart.products);
   else res.status(404).json({ error: 'Carrito no encontrado' });
 });
 
-// POST /api/carts/:cid/product/:pid → agregar producto a carrito
 router.post('/:cid/product/:pid', async (req, res) => {
   const updatedCart = await manager.addProductToCart(req.params.cid, req.params.pid);
   if (updatedCart) res.json(updatedCart);
